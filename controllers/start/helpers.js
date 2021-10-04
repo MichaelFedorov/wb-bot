b77cdccbf4bef804c9c144e23289b21983547343
Aleksei Chyrva <alexey.chyrva@gmail.com>
2021-10-05 01:48:17 +0300

    add methods for FaunaDB connect

--- controllers/start/helpers.js
+++ controllers/start/helpers.js
@@ -1,9 +1,51 @@
+const faunadb = require("faunadb");
+
+const { Select, Get, Match, Identify, Index } = faunadb.query;
+
+let client = new faunadb.Client({ secret: process.env.FDB_FQL_SERVER_KEY });
+
 const regExp = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
 
 const validateEmail = (email) => {
     return regExp.test(email)
 }
 
-module.exports = { 
-    validateEmail 
+const findUserByEmail = async email => {
+    try {
+        const _res = await client.query(
+          Select(["ref"], Get(Match(Index("email"), email)))
+        );
+        return _res;
+    } catch (e) {
+        console.log(e.status);
+    }
+};
+
+const findUserById = async id => {
+    try {
+        const _res = await client.query(
+          Select(["ref"], Get(Match(Index("id"), id)))
+        );
+        return _res;
+    } catch (e) {
+        console.log(e.status);
+    }
+};
+
+const matchUserIdWithAoiKey = async (id, key) => {
+    try {
+        const _id = await client.query(
+          Identify(Match(Index("id"), user), key)
+        );
+        console.log(_id);
+    } catch (e) {
+        console.log(e);
+    }
+};
+
+module.exports = {
+    findUserByEmail,
+    findUserById,
+    matchUserIdWithAoiKey,
+    validateEmail
 }
