c7576cb2b5ce1eced0c620be3db2437a1ffd4183
Aleksei Chyrva <alexey.chyrva@gmail.com>
2021-10-06 00:52:40 +0300

    add helpers

--- controllers/start/helpers.js
+++ controllers/start/helpers.js
@@ -1,6 +1,6 @@
 const faunadb = require("faunadb");
 
-const { Select, Get, Match, Identify, Index } = faunadb.query;
+const { Select, Get, Match, Identify, Index, Create, Collection, Paginate } = faunadb.query;
 
 let client = new faunadb.Client({ secret: process.env.FDB_FQL_SERVER_KEY });
 
@@ -43,9 +43,40 @@ const matchUserIdWithAoiKey = async (id, key) => {
     }
 };
 
+const createUser = async user => {
+  try {
+    const userData = await client.query(
+      Create(
+        Collection('Users'),
+        {
+          data: { ...user },
+        },
+      )
+    );
+    return userData;
+  } catch (e) {
+    console.error(e)
+  }
+};
+
+const validateApiByUserId = async (userId, apiKey) => {
+  try {
+    const keyUsedBy = client.query(
+      Paginate(Match(Index('wbApiKey'), apiKey))
+    )
+    console.log(keyUsedBy)
+    return keyUsedBy?.data?.length > 0;
+  } catch (e) {
+    console.error(e)
+    return false
+  }
+};
+
 module.exports = {
-    findUserByEmail,
-    findUserById,
-    matchUserIdWithAoiKey,
-    validateEmail
+  createUser,
+  findUserByEmail,
+  findUserById,
+  matchUserIdWithAoiKey,
+  validateEmail,
+  validateApiByUserId
 }
