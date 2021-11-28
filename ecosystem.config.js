9d199c5c78b3a9f7198ea7c02889ca22bf1f18bb
Aleksei Chyrva <alexey.chyrva@gmail.com>
2021-11-28 23:32:41 +0200

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -10,6 +10,14 @@ module.exports = {
     },
     env_staging: {
       NODE_ENV: "staging",
+      BOT_TOKEN: '2022873163:AAF9j6zPufl0FhBbzgT90X3XnrLnXfqkTm4',
+      ADMINS_IDS: [],
+      PUBLIC_ID_CLOUD_PAYMENTS: 'pk_f4246e3d8fd85abba6d4306db4aa5',
+      PASSWORD_CLOUD_PAYMENTS: 'c0146e07c84dda268213e0670c271855',
+      DB_NAME: 'wb_bot_test',
+      DB_USER_NANE: 'wbAdmin',
+      DB_PASSWORD: 'gthtrhtcnjr123',
+      DB_HOST: 'localhost:27017'
     },
     env_production: {
       NODE_ENV: "production",
@@ -20,7 +28,7 @@ module.exports = {
       DB_NAME: 'wb_bot_test',
       DB_USER_NANE: 'wbAdmin',
       DB_PASSWORD: 'gthtrhtcnjr123',
-      DB_HOST: '127.0.0.1:27017'
+      DB_HOST: 'localhost:27017'
     }
   }]
 }
